42f198dbad15fa9cb52ae2cc3945759619020d0f
장상옥 <jso46034@naver.com>
2021-04-05 22:47:15 +0900

    Create clock
    1. 시간 함수 생성

---
+++ clock.js
@@ -0,0 +1,28 @@
+const CLOCK_CONTAINER = document.querySelector('.js-clock'), 
+    CLOCK_TITLE = CLOCK_CONTAINER.querySelector('h1');
+
+// 시간을 얻는 함수
+function getTime() {
+    const DATE = new Date();
+    const HOURS = DATE.getHours();
+    const MINUTES = DATE.getMinutes();
+    const SECONDS = DATE.getSeconds();
+    const MILLI_SECONDS = DATE.getMilliseconds();
+
+    CLOCK_TITLE.innerText = `${formatTime(HOURS)}:${formatTime(MINUTES)}:${formatTime(SECONDS)}:${MILLI_SECONDS}`;
+}
+
+// 시,분,초 가 10 이하일 때는 0을 붙여서 return하는 함수
+function formatTime(hms) {
+    if(hms <10) {
+        return `0${hms}`;
+    } else {
+        return hms;
+    }
+};
+
+function init() {
+    setInterval(getTime,1);
+};
+
+init();
