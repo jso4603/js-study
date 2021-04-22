ab605f45afefc6b3b53e378824d7fdb1a1cb452c
장상옥 <jso46034@naver.com>
2021-04-22 18:26:05 +0900

    Javascript Window Resize Study
    1. 화면 사이즈가 변할 때 Window 배경화면이 변하도록 설정

---
+++ js-challenge/challenge1/src/index.js
@@ -0,0 +1,21 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import './styles.css';
+// <⚠️ /DONT DELETE THIS ⚠️>
+const body = document.querySelector('body');
+
+body.style.color = 'white';
+body.style.backgroundColor = '#fdcb6e';
+
+function handleResize(event) {
+    const windowWidth = event.target.innerWidth;
+
+    if (windowWidth <= 700) {
+        body.style.backgroundColor = '#0984e3';
+    } else if (windowWidth <= 1200) {
+        body.style.backgroundColor = '#6c5ce7';
+    } else {
+        body.style.backgroundColor = '#fdcb6e';
+    }
+}
+
+window.addEventListener('resize', handleResize);
